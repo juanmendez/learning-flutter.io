c9a10f89558d71224666526ef2712cb0f23be8f3
Juan Mendez <juan.mendez.3892@gmail.com>
2020-07-26 18:33:58 -0500

    007: centering

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Learning how to make applications in Flutter
 Cards are cousins to Cards in Android. The only difference is there is no padding, so we need to wrap our child into a Padding object.
 We leave a `Row` within first card, but show a `ListTile` which does the same.
 
+Not even my early Android experiments looked this cool!!
+
+![image](https://user-images.githubusercontent.com/3371622/88492170-65d48480-cf6e-11ea-8517-134a5fa67c3c.png)
+
--- lib/main.dart
+++ lib/main.dart
@@ -16,81 +16,70 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: ConstrainedBox(
-            constraints: BoxConstraints.expand(),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: NetworkImage(profileUrl),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "Juan Mendez",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 40.0,
-                      fontFamily: "Pacifico"),
-                ),
-                Text(
-                  "ANDROID DEVELOPER",
-                  style: TextStyle(
-                      color: Colors.teal[100],
-                      fontSize: 20.0,
-                      fontFamily: "Source Sans Pro",
-                      letterSpacing: 1.8,
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 10),
-                Card(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                  child: Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.phone_android,
-                          size: 20,
-                          color: Colors.teal,
-                        ),
-                        SizedBox(width: 10),
-                        Text(
-                          "800 588.2300",
-                          style: TextStyle(
-                              color: Colors.teal.shade900,
-                              fontFamily: "Source Sans Pro"),
-                        )
-                      ],
-                    ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: NetworkImage(profileUrl),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Juan Mendez",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontFamily: "Pacifico"),
+              ),
+              Text(
+                "ANDROID DEVELOPER",
+                style: TextStyle(
+                    color: Colors.teal[100],
+                    fontSize: 20.0,
+                    fontFamily: "Source Sans Pro",
+                    letterSpacing: 1.8,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                width: 300,
+                height: 30,
+                child: Divider(color: Colors.teal.shade100),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone_android,
+                    size: 20,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "800 588.2300",
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: "Source Sans Pro"),
                   ),
                 ),
-                Card(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-                  child: Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.android_sharp,
-                          size: 20,
-                          color: Colors.teal,
-                        ),
-                        SizedBox(width: 10),
-                        Text(
-                          "juan.mendez@github.com",
-                          style: TextStyle(
-                              color: Colors.teal.shade900,
-                              fontFamily: "Source Sans Pro"),
-                        )
-                      ],
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: ListTile(
+                    leading: Icon(
+                      Icons.android_sharp,
+                      size: 20,
+                      color: Colors.teal,
                     ),
-                  ),
-                ),
-              ],
-            ),
+                    title: Text(
+                      "juan.mendez@github.com",
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: "Source Sans Pro"),
+                    )),
+              ),
+            ],
           ),
         ),
       ),
