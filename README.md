3d9ffd4b71fde71bd985d2a694dd98808237f35f
Juan Mendez <juan.mendez.3892@gmail.com>
2020-07-26 18:20:22 -0500

    007: using a row

--- README.md
+++ README.md
@@ -2,15 +2,8 @@
 
 Learning how to make applications in Flutter
 
-## Working with Custom Fonts
+## Working with Cards
 
-Adding custom fonts is easy. The only difficulty is by copying how to embed a custom fonts in YAML.
-There is a strict identation so we need to ensure we have the correct spacing.
-Just remember in a previous chapter adding images in YAML works the same way for fonts. Although now we need to specify the font family, instead of just including the whole directory.
-
-<img width="372" alt="image" src="https://user-images.githubusercontent.com/3371622/88491486-b1842f80-cf68-11ea-9d3f-41fd7bbbbf14.png">
-
-Read more:
- - https://fonts.google.com
- - https://flutter.dev/docs/cookbook/design/fonts
+Cards are cousins to Cards in Android. The only difference is there is no padding, so we need to wrap our child into a Padding object.
+We leave a `Row` within first card, but show a `ListTile` which does the same.
 
--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,8 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final profileUrl = "https://avatars2.githubusercontent.com/u/3371622?s=400&u=9dcf47146ab4693708877582baacc5731ff50855&v=4";
+    final profileUrl =
+        "https://avatars2.githubusercontent.com/u/3371622?s=400&u=9dcf47146ab4693708877582baacc5731ff50855&v=4";
 
     return MaterialApp(
       home: Scaffold(
@@ -30,8 +31,7 @@ class MyApp extends StatelessWidget {
                   style: TextStyle(
                       color: Colors.white,
                       fontSize: 40.0,
-                      fontFamily: "Pacifico"
-                  ),
+                      fontFamily: "Pacifico"),
                 ),
                 Text(
                   "ANDROID DEVELOPER",
@@ -39,10 +39,56 @@ class MyApp extends StatelessWidget {
                       color: Colors.teal[100],
                       fontSize: 20.0,
                       fontFamily: "Source Sans Pro",
-                    letterSpacing: 1.8,
-                    fontWeight: FontWeight.bold
+                      letterSpacing: 1.8,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.phone_android,
+                          size: 20,
+                          color: Colors.teal,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          "800 588.2300",
+                          style: TextStyle(
+                              color: Colors.teal.shade900,
+                              fontFamily: "Source Sans Pro"),
+                        )
+                      ],
+                    ),
                   ),
-                )
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.android_sharp,
+                          size: 20,
+                          color: Colors.teal,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          "juan.mendez@github.com",
+                          style: TextStyle(
+                              color: Colors.teal.shade900,
+                              fontFamily: "Source Sans Pro"),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
