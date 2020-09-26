d6fc19223d338fa6d947dc499d620dc209bcfbd7
Juan Mendez <juan.mendez.3892@gmail.com>
2020-09-26 13:11:09 -0500

    33: modal progress hud

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:learning_flutter/screens/widgets/pill_button.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 import '../constants.dart';
 
@@ -12,75 +13,91 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   String _email, _password;
   FirebaseAuth _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
 
   void _login() {
+    showSpinner = true;
+
     if (_email != null && _password != null) {
       _auth.signInWithEmailAndPassword(
         email: _email,
         password: _password,
       ).then((value) {
+        showSpinner = true;
         if (value.user != null) {
           Navigator.pushNamed(context, Routes.CHAT_ROUTE);
         }
+      }, onError: (error) {
+        _isLoading(false);
       });
     }
+  }
+
+  void _isLoading(bool loading) {
+    setState(() {
+      showSpinner = loading;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('assets/images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('assets/images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                _email = value;
-              },
-              decoration: kInputDecoration.copyWith(
-                hintText: 'Enter your email',
+              SizedBox(
+                height: 48.0,
               ),
-              style: TextStyle(color: Colors.black),
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.emailAddress,
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                _password = value;
-              },
-              decoration: kInputDecoration.copyWith(
-                hintText: 'Enter your password',
+              TextField(
+                onChanged: (value) {
+                  _email = value;
+                },
+                decoration: kInputDecoration.copyWith(
+                  hintText: 'Enter your email',
+                ),
+                style: TextStyle(color: Colors.black),
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
               ),
-              style: TextStyle(color: Colors.black),
-              textAlign: TextAlign.center,
-              obscureText: true,
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            PillButton(
-              'Log In',
-              Colors.lightBlueAccent,
-              _login,
-            ),
-          ],
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  _password = value;
+                },
+                decoration: kInputDecoration.copyWith(
+                  hintText: 'Enter your password',
+                ),
+                style: TextStyle(color: Colors.black),
+                textAlign: TextAlign.center,
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              PillButton(
+                'Log In',
+                Colors.lightBlueAccent,
+                _login,
+              ),
+            ],
+          ),
         ),
+        color: Colors.lightBlueAccent,
       ),
     );
   }
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:learning_flutter/constants.dart';
 import 'package:learning_flutter/screens/widgets/pill_button.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class RegistrationScreen extends StatefulWidget {
   @override
@@ -12,15 +12,27 @@ class RegistrationScreen extends StatefulWidget {
 class _RegistrationScreenState extends State<RegistrationScreen> {
   FirebaseAuth _auth = FirebaseAuth.instance;
   String _email, _password;
+  bool showSpinner = false;
 
   void _register() {
+    showSpinner = true;
+
     _auth.createUserWithEmailAndPassword(
       email: _email,
       password: _password,
     ).then((newUser) {
+      _isLoading(false);
       if (newUser != null) {
         Navigator.pushNamed(context, Routes.CHAT_ROUTE);
       }
+    }, onError: (error) {
+      _isLoading(false);
+    });
+  }
+
+  void _isLoading(bool loading) {
+    setState(() {
+      showSpinner = loading;
     });
   }
 
@@ -28,55 +40,59 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('assets/images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('assets/images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                _email = value;
-              },
-              decoration:
-                  kInputDecoration.copyWith(hintText: 'Enter your email'),
-              style: TextStyle(color: Colors.black),
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.emailAddress,
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                _password = value;
-              },
-              decoration:
-                  kInputDecoration.copyWith(hintText: 'Enter your password'),
-              style: TextStyle(color: Colors.black),
-              textAlign: TextAlign.center,
-              obscureText: true,
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            PillButton(
-              'Register',
-              Colors.blueAccent,
-              _register,
-            ),
-          ],
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  _email = value;
+                },
+                decoration:
+                    kInputDecoration.copyWith(hintText: 'Enter your email'),
+                style: TextStyle(color: Colors.black),
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  _password = value;
+                },
+                decoration:
+                    kInputDecoration.copyWith(hintText: 'Enter your password'),
+                style: TextStyle(color: Colors.black),
+                textAlign: TextAlign.center,
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              PillButton(
+                'Register',
+                Colors.blueAccent,
+                _register,
+              ),
+            ],
+          ),
         ),
+        color: Colors.lightBlueAccent,
       ),
     );
   }
